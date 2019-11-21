1625cb674b62078b1629814f61add3ec414d72e1
Allegra Pusateri <allegrapusateri@gmail.com>
2019-11-20 20:00:50 -0600

    Style homepage, footer, and nag, added background pattern

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -12,12 +12,12 @@ export default class Homepage extends Component {
                 <header className='header margins homepage'>
                     <h1 className='h1_size line_height'>Cover to Cover</h1>
                     <h2 className='h2_size'>read a book. discuss a book.</h2>
-                    <Link to='/clubs' className='link_style' style={{ textDecoration: 'none' }}><p className='home_hyperlink'>find a book club ></p></Link>
+                    <Link to='/clubs' className='link_style home_link' style={{ textDecoration: 'none' }}><p className='home_hyperlink'>find a book club ></p></Link>
                 </header>
 
                 <section className='homepage_body margins'>
                     <p className='home_p'>Looking for fellow book lovers? We can help with that.</p>
-                    <p className='home_p'>At Cover to Cover we take we make book clubbing as easy. All you have to do is find one to join. Can't find one that interests you? Start one yourself. </p>
+                    <p className='home_p'>At Cover to Cover we make book clubbing easy. All you have to do is find one to join. Can't find a club that interests you? Start one yourself. </p>
                     <p className='home_p'>Anyone with account can join or start a club. You can also add a book for the club to read, and contribute to the conversation in the comments.</p>
                 </section>
             </>
